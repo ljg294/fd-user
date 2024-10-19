74f28cd30dde3b8ae553e7f6d9365acb8fa3164e
ljg294 <ljg29494@gmail.com>
2024-10-19 20:12:00 +0900

    USR003 -- Get User Detail API

--- api-server/src/main/kotlin/com/fitdine/user/api/server/controller/UserController.kt
+++ api-server/src/main/kotlin/com/fitdine/user/api/server/controller/UserController.kt
@@ -3,12 +3,16 @@ package com.fitdine.user.api.server.controller
 import com.fitdine.user.api.server.common.response.ApiResponse
 import com.fitdine.user.api.server.common.response.ApiResponseGenerator
 import com.fitdine.user.api.server.dto.reqeust.UserCreateRequest
+import com.fitdine.user.api.server.dto.reqeust.UserDetailResponse
 import com.fitdine.user.api.server.service.UserCommandService
+import com.fitdine.user.api.server.service.UserQueryService
 import jakarta.validation.Valid
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,19 +23,33 @@ import org.springframework.web.bind.annotation.RestController
 @RequiredArgsConstructor
 @Slf4j
 class UserController(
-    private val userCommandService: UserCommandService
+    private val userCommandService: UserCommandService,
+    private val userQueryService: UserQueryService
 ) {
 
     /**
-     * 회원 생성
+     * 사용자 생성
      *
      * @param userCreateRequest
      * @return
      */
     @PostMapping
-    fun createMember(@Valid @RequestBody userCreateRequest: UserCreateRequest): ResponseEntity<ApiResponse<Void>> {
+    fun createUser(@Valid @RequestBody userCreateRequest: UserCreateRequest): ResponseEntity<ApiResponse<Void>> {
         userCommandService.createUser(userCreateRequest)
         val apiResponse = ApiResponseGenerator.success()
+        return ResponseEntity(apiResponse, HttpStatus.OK)
+    }
+
+    /**
+     * 사용자 상세 조회
+     *
+     * @param userId
+     * @return
+     */
+    @GetMapping("'/{userId}")
+    fun findUserDetail(@PathVariable userId: Long): ResponseEntity<ApiResponse<UserDetailResponse?>> {
+        val userDetailResponse = userQueryService.findUserDetail(userId)
+        val apiResponse = ApiResponseGenerator.success(userDetailResponse)
         return ResponseEntity(apiResponse, HttpStatus.OK)
     }
 }
